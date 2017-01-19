fb320bedf05793a8b49d035ea8b00d6ecdc79801
kalahel <mathieu95480@gmail.com>
2017-01-19 17:54:38 +0100

    Finalisation abstract event

--- src/ucp/glp/event/Event.java
+++ src/ucp/glp/event/Event.java
@@ -4,8 +4,14 @@ import ucp.glp.histoire.utilitaires.Peuple;
 
 public abstract class Event {
 	//sprite location  = location + nom + .png
-	protected String description,nom,spritesLocation;
+	protected String nom,spritesLocation;
 	protected int amplitude; //multiplicateur du résultat
+	protected int type;
+	protected double ampReel,puissance;
+	
+	public Event(){
+		genereAmpReel();
+	}
 	
 	public String toString(Peuple p){
 		String amp = "";
@@ -20,12 +26,76 @@ public abstract class Event {
 				amp = "grand(e)";
 				break;
 			default:
-			    System.out.println("ERREUR ENTREE VALEUR EVENT");
+			    System.out.println("ERREUR ENTREE VALEUR EVENT" + this.nom);
 		}
-		
-		
-		return ("un(e) " + amp + " " + this.nom + " touche " + p.getNom());
-		
+		return ("un(e) " + amp + " " + this.nom + " touche " + p.getNom());		
 	}
+	
+	
+	public void genereAmpReel(){	//méthode gérant les différant coeficient multiplicateur
+		switch(amplitude){
+		case 0:
+			ampReel = 0.5;
+			break;
+		case 1 :
+			ampReel = 1;
+			break;
+		case 2 :
+			ampReel = 2;
+			break;
+		default:
+		    System.out.println("ERREUR ENTREE genereAmpReel EVENT" + this.nom);
+		}
+	}
+	
+	
+	
+	public void action(Peuple p){
+		switch(type){
+			case 0 :
+				p.setRessources(p.getRessources() + (puissance * ampReel));
+				break;
+			case 1 :
+				p.setPopulation(p.getPopulation() + (int)(puissance * ampReel));
+				break;
+			case 2 : 
+				p.setAgressivite(p.getAgressivite() + (puissance * ampReel));
+				break;
+			case 3 :
+				p.setEducation(p.getEducation() + (puissance * ampReel));
+				break;
+			case 4 :
+				p.setTerritoire(p.getTerritoire() + (puissance * ampReel));
+				break;
+			default:
+			    System.out.println("ERREUR ENTREE action EVENT" + this.nom);
+		
+		}
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getSpritesLocation() {
+		return spritesLocation;
+	}
+
+	public void setSpritesLocation(String spritesLocation) {
+		this.spritesLocation = spritesLocation;
+	}
+
+	public int getAmplitude() {
+		return amplitude;
+	}
+
+	public void setAmplitude(int amplitude) {
+		this.amplitude = amplitude;
+	}
+	
 
 }
