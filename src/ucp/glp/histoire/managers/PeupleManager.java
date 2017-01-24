9737966a98107653ffa27d958f6bda92a895408a
kalahel <mathieu95480@gmail.com>
2017-01-24 13:30:39 +0100

    Ajout peuple manager

---
+++ src/ucp/glp/histoire/managers/PeupleManager.java
@@ -0,0 +1,15 @@
+package ucp.glp.histoire.managers;
+
+import java.util.ArrayList;
+
+import ucp.glp.histoire.utilitaires.Peuple;
+
+public class PeupleManager {
+	
+	public void genereEnsembleTotal(ArrayList<Peuple> listePeuple){
+		for(int i = 0;i < listePeuple.size();i++){
+			listePeuple.get(i).genereEnsemble();			
+		}
+	}
+
+}
