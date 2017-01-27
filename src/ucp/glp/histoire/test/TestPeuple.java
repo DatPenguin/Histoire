1cddfcb4df0f0c70e7de8d2e9a26b5076f1e23df
kalahel <mathieu95480@gmail.com>
2017-01-27 11:26:30 +0100

    Ajout chance manager Event

--- src/ucp/glp/histoire/test/TestPeuple.java
+++ src/ucp/glp/histoire/test/TestPeuple.java
@@ -2,6 +2,7 @@ package ucp.glp.histoire.test;
 
 import java.util.ArrayList;
 
+import ucp.glp.histoire.managers.EventChanceManager;
 import ucp.glp.histoire.managers.EventLocalManager;
 import ucp.glp.histoire.managers.GuerreManager;
 import ucp.glp.histoire.managers.PeupleManager;
@@ -13,22 +14,22 @@ public class TestPeuple {
 	public static void main(String[] args) {
 		Peuple p1 = new Peuple(90,40,10,70,10,"belge");
 		Peuple p2 = new Peuple(75,10,10,25,22,"Grec");
-		EventLocalManager ELM = new EventLocalManager();
-		
+		EventChanceManager ecm = new EventChanceManager();
 		ArrayList<Peuple> listePeuple = new ArrayList<Peuple>();
 		listePeuple.add(p1);
 		
-		for(int i=0; i < 3; i++){
-		System.out.println(p1.toString());
-		PeupleManager.genereEnsembleTotal(listePeuple);
-		ELM.action(p1);
-		System.out.println(p1.toString());
+		for(int i=0; i < 50; i++){
+			//System.out.println(p1.toString());
+			PeupleManager.genereEnsembleTotal(listePeuple);
+			ecm.action(listePeuple);
+			//System.out.println(p1.toString());
 
-		GuerreManager.guerreChecker(listePeuple);
-		TradeManager.TradeChecker(listePeuple);
-		System.out.println(p1.toString());
-		PeupleManager.genereEnsembleTotal(listePeuple);
-		System.out.println(p1.toString());
+			GuerreManager.guerreChecker(listePeuple);
+			TradeManager.TradeChecker(listePeuple);
+			//System.out.println(p1.toString());
+			PeupleManager.genereEnsembleTotal(listePeuple);
+			//System.out.println(p1.toString());
+			System.out.println(i + "");
 		}
 
 		
